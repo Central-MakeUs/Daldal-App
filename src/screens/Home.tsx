481840f0b631555593247d2bccabc37621aabdb4
coddingyun <igy2840@gmail.com>
2024-01-22 23:23:26 +0900

    feat: react-native-webview 설치 및 기본 세팅

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,21 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import WebView from 'react-native-webview';
 
 const Home = () => {
   return (
-    <View>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
-      <Text>haha123</Text>
+    <View style={styles.container}>
+      <WebView source={{uri: 'http://192.168.0.83:5173'}} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Home;
