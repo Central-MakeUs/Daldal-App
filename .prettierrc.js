299dd75d1c4c9d47d05d319a7c2f11a908b4a168
coddingyun <igy2840@gmail.com>
2024-01-23 21:06:14 +0900

    chore: prettier 적용

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,23 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+
+  endOfLine: "auto",
+	tabWidth: 2,
+	semi: true,
+	printWidth: 80,
+	jsxSingleQuote: false,
+	quoteProp: "as-needed",
+	useTabs: true,
+	htmlWhitespaceSensitivity: "css",
+	embeddedLanguageFormatting: "auto",
+	insertPragma: false,
+	requirePragma: false,
+	vueIndentScriptAndStyle: false,
+	overrides: [
+		{
+			files: ".prettierrc",
+			options: { "parser": "json" }
+		}
+	]
 };
