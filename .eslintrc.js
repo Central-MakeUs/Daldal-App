9559e88c9b7b0a0d803e14edb067c890a7e7351e
coddingyun <igy2840@gmail.com>
2024-01-23 19:21:24 +0900

    chore: eslint 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unstable-nested-components': 'off',
+  },
 };
