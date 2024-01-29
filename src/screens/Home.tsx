690af0184c145d3bffb87cda00390764ece47e95
coddingyun <igy2840@gmail.com>
2024-01-29 22:02:17 +0900

    feat: 모달 창 제거

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,30 +1,16 @@
 import {StyleSheet, View} from 'react-native';
 import WebView from 'react-native-webview';
-import {Alert, BackHandler} from 'react-native';
+import {BackHandler} from 'react-native';
 import {useEffect, useRef, useState} from 'react';
 
-type navStateType = {
+type NavStateType = {
   url: string;
   canGoBack: boolean;
 };
 
-const close = () => {
-  Alert.alert('정말 종료하시겠어요?', '앱을 종료하려면 확인을 눌러주세요.', [
-    {
-      text: '취소',
-      onPress: () => {},
-      style: 'cancel',
-    },
-    {
-      text: '확인',
-      onPress: () => BackHandler.exitApp(),
-    },
-  ]);
-};
-
 const Home = () => {
   const webViewRef = useRef<WebView>(null);
-  const [navState, setNavState] = useState<navStateType>({
+  const [navState, setNavState] = useState<NavStateType>({
     url: '',
     canGoBack: false,
   });
@@ -32,16 +18,10 @@ const Home = () => {
   useEffect(() => {
     const handleBackButton = () => {
       if (navState.canGoBack) {
-        //TODO 실배포 주소로 변경
-        if (navState.url === 'http://localhost:5173/') {
-          close();
-        } else {
-          webViewRef.current?.goBack();
-        }
-      } else {
-        close();
+        webViewRef.current?.goBack();
+        return true;
       }
-      return true;
+      return false;
     };
 
     BackHandler.addEventListener('hardwareBackPress', handleBackButton);
